d2bc6441d5b9d8e89cbf7c78e851930385b52d74
Isa Wang <74472229+IsaWang-05@users.noreply.github.com>
2023-12-12 18:26:24 -0500

    Updated line graph skeleton

--- static/scripts.js
+++ static/scripts.js
@@ -53,6 +53,14 @@ $(document).on('click', '.view-details', function(e){
                 values.push($tds.eq(6).text());
             });
 
+            // Create graph data and render the graph
+            var graphData = dates.map(function(date, index) {
+                return {
+                    date: date,
+                    price: parseFloat(prices[index])
+                };
+            });
+
             // Adding the collected data to the table as rows
             dataTable += '<tr><td><strong>Date</strong></td>';
             dates.forEach(function(date) {
@@ -81,18 +89,64 @@ $(document).on('click', '.view-details', function(e){
             // Closing the table tags
             dataTable += '</tbody></table>';
 
-            // Combine topRow, secondRow, and dataTable to form the final modal content
-            var modalContent = topRow + secondRow + dataTable;
-            console.log("Final modal content built: ", modalContent);
+            // Placeholder for the graph
+            var graphPlaceholder = '<div id="graph-container"></div>';
 
-            
+            // Combine topRow, secondRow, dataTable, and graphPlaceholder to form the final modal content
+            var modalContent = topRow + secondRow + dataTable + graphPlaceholder;
+
             setTimeout(function() {
                 $('#dataModal .modal-body').html(modalContent);
                 $('#dataModal').modal('show');
-            }, 1);
+
+                // Now call to render the line graph
+                renderLineGraph(graphData);
+            }, 20);
         },
         error: function(error){
             console.log("Error in AJAX request: ", error);
         }
     });
 });
+
+// Function to render the line graph
+function renderLineGraph(data) {
+    // dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // Append the svg object to a div in your modal
+    var svg = d3.select("#graph-container")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add X axis
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return new Date(d.date); }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.price; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(new Date(d.date)); })
+        .y(function(d) { return y(d.price); })
+      );
+}
