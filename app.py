a550e887d06617ee850b12fab8b3d47b0c4fcbbb
Isa Wang <74472229+IsaWang-05@users.noreply.github.com>
2023-12-12 02:31:54 -0500

    updated db logic

--- app.py
+++ app.py
@@ -5,23 +5,31 @@ class ExcelReader:
     def __init__(self, filename):
         self.filename = filename
 
-    def read_partial(self, issuer=None, symbol=None, deal_team_contact=None):
-        data = pd.read_excel(self.filename, engine='openpyxl', usecols=['issuer', 'symbol', 'fund', 'deal_team_contact', 'curr_price_1'])
+    def read(self, Issuer=None, Symbol=None, Deal_team_contact=None):
+        data = pd.read_excel(self.filename, engine='openpyxl')
         
-        if issuer:
-            data = data[data['issuer'].str.contains(issuer, case=False, na=False)]
-        if symbol:
-            data = data[data['symbol'].str.contains(symbol, case=False, na=False)]
-        if deal_team_contact:
-            data = data[data['deal_team_contact'].str.contains(deal_team_contact, case=False, na=False)]
+        if Issuer:
+            data = data[data['Issuer'].str.contains(Issuer, case=False, na=False)]
+        if Symbol:
+            data = data[data['Symbol'].str.contains(Symbol, case=False, na=False)]
+        if Deal_team_contact:
+            data = data[data['Deal_team_contact'].str.contains(Deal_team_contact, case=False, na=False)]
+
+        # show only the newest record for each symbol
+        data = data.sort_values('Date', ascending=False).drop_duplicates('Symbol')
 
         data['Details'] = data.apply(lambda row: f"<a href='#' class='view-details' data-id='{row.name}'>View Details</a>", axis=1)
         return data.to_html(index=False, escape=False)
 
-    def read_full(self, row_id):
+    def read_hist(self, Symbol):
         data = pd.read_excel(self.filename, engine='openpyxl')
-        row_data = data.iloc[[row_id]]
-        return row_data.to_html(index=False)
+        data = data[data['Symbol'] == Symbol]
+
+        # sort all records by date (earliest to latest)
+        data = data.sort_values('Date', ascending=True)
+
+        return data.to_html(index=False, escape=False)
+
 
 class ExcelNavigatorApp:
     def __init__(self, excel_reader):
@@ -30,18 +38,26 @@ class ExcelNavigatorApp:
 
         @self.app.route('/', methods=['GET'])
         def index():
-            issuer = request.args.get('issuer')
-            symbol = request.args.get('symbol')
-            deal_team_contact = request.args.get('deal_team_contact')
+            Issuer = request.args.get('Issuer')
+            Symbol = request.args.get('Symbol')
+            Deal_team_contact = request.args.get('Deal_team_contact')
 
-            table_html = self.excel_reader.read_partial(issuer, symbol, deal_team_contact)
+            table_html = self.excel_reader.read(Issuer, Symbol, Deal_team_contact)
             return render_template('index.html', table_html=table_html)
 
         @self.app.route('/details', methods=['POST'])
-        def details():
-            row_id = request.json.get('row_id')
-            full_data = self.excel_reader.read_full(int(row_id))
-            return jsonify(full_data)
+               
+        def get_details_by_symbol():
+            data = request.get_json()
+            row_id = data['row_id']
+
+            full_data = pd.read_excel(excel_reader.filename, engine='openpyxl')
+            symbol = full_data.iloc[row_id]['Symbol']
+
+            filtered_html_data = excel_reader.read_hist(Symbol=symbol)
+
+            return jsonify(filtered_html_data)
+
 
 if __name__ == '__main__':
     filepath = './data/starter_db.xlsx'
--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,14 @@
-$(document).ready(function(){
-    $(document).on('click', '.view-details', function(e){
-        e.preventDefault();
-        var rowId = $(this).data('id');
-        $.ajax({
-            url: '/details',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ row_id: rowId }),
-            success: function(response){
-                // wrap the response in a div with class 'table-responsive'
-                var htmlContent = '<div class="table-responsive">' + response + '</div>';
-                $('#dataModal .modal-body').html(htmlContent);
-                $('#dataModal').modal('show');
-            }
-        });
+$(document).on('click', '.view-details', function(e){
+    e.preventDefault();
+    var rowId = $(this).data('id');
+    $.ajax({
+        url: '/details',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ row_id: rowId }),
+        success: function(response){
+        },
+        error: function(error){
+        }
     });
 });
